9c2c94ff473e3b38ada6ca7a8b9ddbc9a317150f
pingec-automation <95188980+pingec-automation@users.noreply.github.com>
2021-11-28 23:41:49 +0100

    .

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,13 @@
 
 # store credentials on disk https://stackoverflow.com/a/56058897
 # git config (--global?) credential.helper store
+
+# stash local changes, update repo to latest version
+git stash
+git pull
+git stash apply
+
+# push local changes
 git add .
 git commit -m "log update"
 git push origin main
